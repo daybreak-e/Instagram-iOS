fdf43b6532d7d1f27322a44b87b5e5329a459291
daybreak-e <scja1704@naver.com>
2021-09-07 23:39:01 +0900

    Finishing Up Feed Cell UI

--- Instagram-iOS/Instagram-iOS/Controller/FeedController.swift
+++ Instagram-iOS/Instagram-iOS/Controller/FeedController.swift
@@ -39,6 +39,11 @@ extension FeedController {
 //MARK: - UICollectionViewDelegateFlowLayout
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        
+        let width = view.frame.width
+        var height = width + 8 + 40 + 8
+        height += 110
+        
+        return CGSize(width: width, height: height)
     }
 }
