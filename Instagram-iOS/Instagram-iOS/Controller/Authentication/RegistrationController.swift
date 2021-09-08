3b8f2e14881d9320390418123490be13d9d89b0d
daybreak-e <scja1704@naver.com>
2021-09-08 22:26:52 +0900

    Login Page UI

---
+++ Instagram-iOS/Instagram-iOS/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationController.swift
+//  Instagram-iOS
+//
+//  Created by 이새벽 on 2021/09/08.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+}
+
