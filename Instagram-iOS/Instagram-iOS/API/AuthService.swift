db0e7d485337067db6f6529f5fb15c262e3b771b
daybreak-e <scja1704@naver.com>
2021-10-02 02:27:47 +0900

    Image Uploader & Authentication Service

---
+++ Instagram-iOS/Instagram-iOS/API/AuthService.swift
@@ -0,0 +1,23 @@
+//
+//  AuthService.swift
+//  Instagram-iOS
+//
+//  Created by 이새벽 on 2021/10/02.
+//
+
+import UIKit
+import Firebase
+
+struct AuthCredentials {
+    let email: String
+    let password: String
+    let fullname: String
+    let username: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    static func registerUser(withCredential credentials: AuthCredentials) {
+        
+    }
+}
